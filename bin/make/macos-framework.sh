3c2962e9676af85ceeca0b5f196cf0b90d1a9316
Joshua Moody <joshuajmoody@gmail.com>
2016-11-15 13:26:02 +0100

    Make: treat warnings as errors
    @sapieneptus Good catch
    
    https://github.com/calabash/CocoaLumberjack/pull/1

--- bin/make/macos-framework.sh
+++ bin/make/macos-framework.sh
@@ -39,7 +39,7 @@ xcrun xcodebuild \
   -target ${XC_TARGET} \
   -configuration ${XC_CONFIG} \
   -sdk macosx \
-  GCC_TREAT_WARNINGS_AS_ERRORS=NO \
+  GCC_TREAT_WARNINGS_AS_ERRORS=YES \
   GCC_GENERATE_TEST_COVERAGE_FILES=NO \
   GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=NO \
   build | $XC_PIPE
